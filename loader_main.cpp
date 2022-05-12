a75705b03dafa37864f4cf25212d31fd8c6d3ca2
SinaMegapolis <sinasaffari6@gmail.com>
2022-05-12 22:01:20 +0430

    Initial Commit

---
+++ loader_main.cpp
@@ -0,0 +1,11 @@
+#include "loader_main.h"
+
+int idaapi accept_file(qstring* fileformatname, qstring* processor, linput_t* li, const char* filename)
+{
+	return 0;
+}
+
+void idaapi load_file(linput_t* li, ushort neflags, const char* fileformatname)
+{
+	
+}
